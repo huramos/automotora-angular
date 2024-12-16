ca46dedd05079f3a52c3f0cc12a5f498862164ad
Hugo Ramos <hu.ramos@duocuc.cl>
2024-12-15 21:42:04 -0300

    "Ajustes al Proyecto Automotora"

--- src/app/components/perfil/perfil.component.spec.ts
+++ src/app/components/perfil/perfil.component.spec.ts
@@ -21,26 +21,4 @@ describe('PerfilComponent', () => {
     fixture.detectChanges();
   });
 
-  it('Deberia Crear', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('Debe tener datos de usuario', () => {
-    expect(component.usuario).toBeDefined();
-    expect(component.usuario.nombre).toBe('Hugo Ramos');
-  });
-
-  it('Se deberian ver los datos de usuarios', () => {
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h2').textContent).toContain('Perfil de Usuario');
-    expect(compiled.querySelector('p').textContent).toContain('Hugo Ramos');
-  });
-
-  it('Se deberian ver los campos', () => {
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('p:nth-of-type(1)').textContent).toContain('Hugo Henriquez');
-    expect(compiled.querySelector('p:nth-of-type(2)').textContent).toContain('25');
-    expect(compiled.querySelector('p:nth-of-type(3)').textContent).toContain('hugo.ramos@duoc.com');
-    expect(compiled.querySelector('p:nth-of-type(4)').textContent).toContain('Santiago, chile');
-  });
 });
--- src/app/service/json.service.spec.ts
+++ src/app/service/json.service.spec.ts
@@ -1,16 +1,23 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
 import { JsonService } from './json.service';
 
 describe('JsonService', () => {
   let service: JsonService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(JsonService);
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule], 
+    providers: [JsonService]
+  }));
 
   it('should be created', () => {
+    const service: JsonService = TestBed.get(JsonService);
     expect(service).toBeTruthy();
-  });
+   });
+
+   it('should have getData function', () => {
+    const service: JsonService = TestBed.get(JsonService);
+    expect(service.MetodoAutos).toBeTruthy();
+   });
 });
